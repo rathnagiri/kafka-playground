26d56035fda8ac4f0df85a5070c957b5534741a8
Giri <rathnagiri@gmail.com>
2024-11-29 14:10:31 +0000

    Created docker-compose file

--- EmployeeApplicationApi/EmployeeApplicationApi/Program.cs
+++ EmployeeApplicationApi/EmployeeApplicationApi/Program.cs
@@ -1,11 +1,12 @@
 using EmployeeApplicationApi.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<EmployeeDbContext>();
+builder.Services.AddDbContext<EmployeeDbContext>(op => op.UseInMemoryDatabase("EmployeesDb"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
