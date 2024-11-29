94fa9eb300b77189ff9868faa7cbd3e066172ec1
Giri <rathnagiri@gmail.com>
2024-11-29 19:58:52 +0000

    finally got it working

--- EmployeeApplicationApi/EmployeeApplicationApi/Controllers/EmployeesController.cs
+++ EmployeeApplicationApi/EmployeeApplicationApi/Controllers/EmployeesController.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using Confluent.Kafka;
 using EmployeeApplicationApi.Database;
 using EmployeeApplicationApi.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -34,6 +36,37 @@ namespace EmployeeApplicationApi.Controllers
             var employee = new Employee(Guid.NewGuid(), name, surname);
             _dbContext.Employees.Add(employee);
             await _dbContext.SaveChangesAsync();
+            var serialize = JsonSerializer.Serialize(employee);
+            _logger.LogInformation($"saving employee {serialize}");
+
+            // message to kafka
+            
+            var message = new Message<string, string>()
+            {
+                Key = employee.Id.ToString(),
+                Value = serialize
+            };
+
+            // client
+            var producerConfig = new ProducerConfig()
+            {
+                BootstrapServers = "kafka:9092",
+                Acks = Acks.All
+            };
+
+            // producer
+            using (var producer = new ProducerBuilder<string, string>(producerConfig).Build())
+            {
+                try
+                {
+                    var deliveryResult = await producer.ProduceAsync("employeeTopic", message);
+                    _logger.LogInformation($"Delivered message to Topic: {deliveryResult.Topic} | Partition : {deliveryResult.TopicPartition} | value {deliveryResult.Value}");
+                }
+                catch (ProduceException<string, string> e)
+                {
+                    _logger.LogError($"Delivery Failed: {e.Error.Reason}");
+                }
+            }
 
             return CreatedAtAction(nameof(CreateEmployee), new { id = employee.Id }, employee);
         }
